7ac78aa77e48226f6d0f7a6b7f714c8e67d22daf
Mike Hovedskov <deengo113@gmail.com>
2024-05-08 22:32:33 +0200

    Added docker files and simple compose
    Still missing swarm setup and readme

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,25 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  preview: {
+    port: 5173,
+    strictPort: true,
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: true,
+    origin: "https://0.0.0.0:5173",
+  },
+});
